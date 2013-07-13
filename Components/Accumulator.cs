b0eeab8d5000578c4fe7142b3d3915ee4b1f626f
John McDonald <jcpmcdonald@gmail.com>
2013-07-13 11:09:11 -0600

    Bug fixed: Floating text will now float up again. My JSON populate function didn't fail fast when trying to set a private member, instead just laughed at me (quietly)

--- Components/Accumulator.cs
+++ Components/Accumulator.cs
@@ -36,7 +36,7 @@ namespace AsteroidOutpost.Components
 
 		public int Value { get; set; }
 		public Vector2 Offset { get; set; }
-		public Vector2 Velocity { get; private set; }
+		public Vector2 Velocity { get; set; }
 		public float FadeRate { get; set; }
 		public Color Color { get; set; }
 	}
