3f00d7f557bc3e576864e07bd64a08abb71c31f9
John McDonald <jcpmcdonald@gmail.com>
2013-08-03 15:49:06 -0600

    Forgot to add the Targetable component

---
+++ Components/Targetable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsteroidOutpost.Components
+{
+	public class Targetable : Component
+	{
+		public Targetable(int entityID)
+			: base(entityID)
+		{
+		}
+	}
+}
